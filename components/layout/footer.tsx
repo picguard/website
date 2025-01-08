f06961433c208ac5bbd940b6c2e9d9650287fa91
kjxbyz <kjxbyz888@outlook.com>
2025-01-08 19:57:20 +0800

    fix: update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -12,17 +12,17 @@ function Footer(props: LngProps) {
 
   return (
     <div className="w-full border-b border-gray-200 py-5 text-center dark:border-gray-700">
-      <p className="text-gray-500 dark:text-white/80">
-        {t("footer")}&nbsp;
-        <a
-          className="font-medium text-gray-800 underline transition-colors dark:text-white/90"
-          href="https://github.com/picguard/websites"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+      {/*<p className="text-gray-500 dark:text-white/80">*/}
+      {/*  {t("footer")}&nbsp;*/}
+      {/*  <a*/}
+      {/*    className="font-medium text-gray-800 underline transition-colors dark:text-white/90"*/}
+      {/*    href="https://github.com/picguard/websites"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    GitHub*/}
+      {/*  </a>*/}
+      {/*</p>*/}
       <p className="mt-2 flex items-center justify-center">
         <Link
           className="font-medium text-gray-800 underline transition-colors dark:text-white/90"
@@ -42,10 +42,7 @@ function Footer(props: LngProps) {
       </p>
       <span className="mt-2 flex flex-wrap items-center justify-center text-sm text-gray-500 dark:text-gray-400 sm:text-center">
         &copy;&nbsp;{`2023${fullYear === 2023 ? "" : `-${fullYear}`}`}&nbsp;
-        <Link href={`/${props.lng}`} rel="noopener noreferrer">
-          {th("title")}
-        </Link>
-        .&nbsp;{t("copyright")}&nbsp;
+        {th("title")}.&nbsp;{t("copyright")}&nbsp;
         {VERCEL_GIT_COMMIT_SHA && (
           <>{VERCEL_GIT_COMMIT_SHA.substring(0, 8)}&nbsp;</>
         )}
