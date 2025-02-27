07cdda4f92143c6859e2f6c44f3940effb6b4298
kjxbyz <kjxbyz888@outlook.com>
2025-02-27 20:14:22 +0800

    fix: update envs

--- process.d.ts
+++ process.d.ts
@@ -1,10 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NEXT_PUBLIC_GIT_COMMIT_SHA: string;
-    NEXT_PUBLIC_GOOGLE_ID: string;
-    NEXT_PUBLIC_COOKIE_BANNER_ID: string;
-    NEXT_PUBLIC_SHOW_PARTICLES: string;
-    NEXT_PUBLIC_WEBSITE_GLOBAL_GRAY: string;
+    GIT_COMMIT_SHA: string;
+    GOOGLE_ID: string;
+    COOKIE_BANNER_ID: string;
+    SHOW_PARTICLES: string;
+    WEBSITE_GLOBAL_GRAY: string;
     GH_TOKEN: string;
     GH_REPO_OWNER: string;
     GH_REPO: string;
