0f542fd18c52b53be371500bfb8ae8e9b70dffa9
kjxbyz <kjxbyz@users.noreply.github.com>
2025-04-08 08:11:44 +0000

    deploy: 155b4bb47ea9b69411a999b28309408909932468

--- _next/static/chunks/app/[lng]/not-found-290087f6ccae30c8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9600],{26377:function(n,e,u){Promise.resolve().then(u.t.bind(u,22007,23))}},function(n){n.O(0,[2007,8825,622,1744],function(){return n(n.s=26377)}),_N_E=n.O()}]);
