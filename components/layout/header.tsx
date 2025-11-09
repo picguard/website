6cede314ea0c8ec42d6c344c1a427856d266ebbb
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2025-11-09 17:57:49 +0800

    fix: update domains

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -59,7 +59,7 @@ export default function Header(props: LngProps) {
                   <Tooltip>
                     <TooltipTrigger>
                       <Link
-                        href="https://www.picguard.app/play/"
+                        href="https://web.picguard.app"
                         className="flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
                       >
                         <LuExternalLink className="h-5 w-5" />
